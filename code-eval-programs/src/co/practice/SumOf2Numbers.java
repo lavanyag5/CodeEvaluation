073a4088aeeac84777096183c268473b7f594a7a
lavanyag5 <lavanyagovinda1995@gmail.com>
2022-03-10 16:21:28 +0530

    Sum of 2 numbers

---
+++ code-eval-programs/src/co/practice/SumOf2Numbers.java
@@ -0,0 +1,14 @@
+package co.practice;
+
+public class SumOf2Numbers {
+
+	public long sum(int a, int b) {
+		long sum;
+		return sum = a + b;
+	}
+
+	public static void main(String[] args) {
+		SumOf2Numbers sum2 = new SumOf2Numbers();
+		System.out.println("Result : " + sum2.sum(4, 8));
+	}
+}
