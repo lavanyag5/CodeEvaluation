0678d437586230b6c6503fd6158a097a0e9f60d0
lavanyag5 <lavanyagovinda1995@gmail.com>
2022-03-15 23:27:28 +0530

    First and Last character exchange

---
+++ code-eval-programs/src/co/practice/FirstAndLastCharChanged.java
@@ -0,0 +1,24 @@
+/*
+ * 10. Given a String, return a new string where the 1st and last chars have been changed.
+ * Ex: input -> code, output ->edoc
+ */
+
+package co.practice;
+
+public class FirstAndLastCharChanged {
+
+	public String frontBackString(String str) {
+		String result = " ";
+		result = str.charAt(str.length() - 1) + str.substring(1, str.length() - 1) + str.charAt(0);
+		return result;
+	}
+
+	public static void main(String[] args) {
+		FirstAndLastCharChanged charChanged = new FirstAndLastCharChanged();
+		System.out.println("Result :" + charChanged.frontBackString("abcd"));
+	}
+}
+
+//OUTPUT
+// Scenario 1 -> input 1 -> abcd -> result : dbca
+// Scenario 2 -> input 2 -> code -> result : eodc
