d0a76503bd198989e987f8e5150e0d9c98d399c9
lavanyag5 <lavanyagovinda1995@gmail.com>
2022-03-15 15:45:04 +0530

    Multiple of 3 or 5

--- code-eval-programs/src/co/practice/MultipleOf3or5.java
+++ code-eval-programs/src/co/practice/MultipleOf3or5.java
@@ -1,5 +1,5 @@
 /*
- * Return true if the given non-negative number is a multiple of 3 or a multiple of 5.Use % operator.
+ * 4. Return true if the given non-negative number is a multiple of 3 or a multiple of 5.Use % operator.
  */
 
 package co.practice;
